e516fe6914413a1f14577b046435e07e0d6b4dcc
CodeGuru15 <arijitpal6111@gmail.com>
2023-11-24 00:21:54 +0530

    FormHandle updated

--- client/my-app/src/components/InputSignUp.js
+++ client/my-app/src/components/InputSignUp.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./style.css";
 import { FaUser } from "react-icons/fa";
 import { FaMobile } from "react-icons/fa6";
@@ -5,47 +6,83 @@ import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 
 const Inputs = () => {
+  const [data, SetData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const user = e.target.name.value;
+    const contact = e.target.contact.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    SetData({
+      name: user,
+      contact: contact,
+      email: email,
+      password: password,
+    });
+    console.log(data);
+  };
+
   return (
     <div className="m-1 grid border-indigo-300 text-center sm:border-r-2 sm:pr-5">
-      <div>
-        <label>
-          <FaUser />
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">
+          <i>
+            <FaUser />
+          </i>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+            autoComplete="off"
+            required="true"
+            className="m-1 border-b border-black p-1"
+          ></input>
         </label>
-        <input
-          placeholder="Enter your name"
-          className="m-1 border-b border-black p-1"
-        ></input>
-      </div>
-      <div>
-        <label>
+        <label htmlFor="contact">
           <FaMobile />
+          <input
+            type="number"
+            name="contact"
+            id="contact"
+            placeholder="Contact No."
+            autoComplete="off"
+            className="m-1 border-b border-black p-1"
+          ></input>
         </label>
-        <input
-          placeholder="Mobile No."
-          className="m-1 border-b border-black p-1"
-        ></input>
-      </div>
-      <div>
-        <label>
+        <label htmlFor="email">
           <MdEmail />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            autoComplete="off"
+            required="true"
+            className="m-1 border-b border-black p-1"
+          ></input>
         </label>
-        <input
-          placeholder="Email"
-          className="m-1 border-b border-black p-1"
-        ></input>
-      </div>
-      <div>
-        <label>
+        <label htmlFor="password">
           <RiLockPasswordFill />
+          <input
+            name="password"
+            id="password"
+            placeholder="Password"
+            autoComplete="off"
+            required="true"
+            className="m-1 border-b border-black p-1"
+          ></input>
         </label>
-        <input
-          placeholder="Password"
-          className="m-1 border-b border-black p-1"
-        ></input>
-      </div>
-      <button className="mb-2 mt-5 rounded-2xl border-2 border-transparent bg-blue-500 px-5 py-1  text-white hover:text-black">
-        Sign Up
-      </button>
+        <div>
+          <button
+            type="submit"
+            className="mb-2 mt-5 rounded-2xl border-2 border-transparent bg-blue-500 px-5 py-1  text-white hover:text-black"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
